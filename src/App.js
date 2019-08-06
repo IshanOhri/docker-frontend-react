1534b2e8dc98df3c93f58250fb5182d83be0625e
ishan <ishan.ohri.7@gmail.com>
2019-08-06 14:57:28 -0400

    updated app.js

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ class App extends Component {
           <h2>Welcome to React</h2>
         </div>
         <p className="App-intro">
-          Code pushed to Travis via feature and also deploy on aws.
+          Code pushed to Travis.
         </p>
       </div>
     );
